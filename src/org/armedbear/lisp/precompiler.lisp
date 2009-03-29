7e45966d6a00d42447e1bdb15f4bba734b44b2b5
ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2009-03-29 09:52:14 +0000

    Fix unbound variable error.

--- src/org/armedbear/lisp/precompiler.lisp
+++ src/org/armedbear/lisp/precompiler.lisp
@@ -1124,7 +1124,12 @@
     (let* ((block-name (fdefinition-block-name name))
            (lambda-expression `(named-lambda ,name ,lambda-list ,@decls ,@(when doc `(,doc))
                                              (block ,block-name ,@body))))
-      (cond (*file-compilation*
+      (cond ((and (boundp 'jvm::*file-compilation*)
+                  ;; when JVM.lisp isn't loaded yet, this variable isn't bound
+                  ;; meaning that we're not trying to compile to a file:
+                  ;; Both COMPILE and COMPILE-FILE bind this variable.
+                  ;; This function is also triggered by MACROEXPAND, though
+                  jvm::*file-compilation*)
              `(fset ',name ,lambda-expression))
             (t
              (when (and env (empty-environment-p env))
