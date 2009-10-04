8c20ec28f6ca0a03c2a1feae3671b051fdf1b865
ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2009-10-04 14:01:58 +0000

    Fix dispatch() generic case. Fixes compiled MULTIPLE-VALUE-CALL with more than 8 arguments.
    Pointed out by piso.

--- src/org/armedbear/lisp/LispObject.java
+++ src/org/armedbear/lisp/LispObject.java
@@ -761,7 +761,7 @@ public class LispObject extends Lisp
         return execute(args[0], args[1], args[2], args[3], args[4],
                        args[5], args[6], args[7]);
       default:
-        return type_error(this, Symbol.FUNCTION);
+        return execute(args);
       }
   }
 
