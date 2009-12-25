09b8d5a8518597b9b7dcb9b7fd1fefbc5e75e85f
ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2009-12-25 22:34:53 +0000

    Turn an inline named class name into a string constant.

--- src/org/armedbear/lisp/compiler-pass2.lisp
+++ src/org/armedbear/lisp/compiler-pass2.lisp
@@ -220,6 +220,8 @@
 (defconstant +lisp-fixnum-class+ "org/armedbear/lisp/Fixnum")
 (defconstant +lisp-fixnum+ "Lorg/armedbear/lisp/Fixnum;")
 (defconstant +lisp-fixnum-array+ "[Lorg/armedbear/lisp/Fixnum;")
+(defconstant +lisp-function-proxy-class+
+  "org/armedbear/lisp/AutoloadedFunctionProxy")
 (defconstant +lisp-bignum-class+ "org/armedbear/lisp/Bignum")
 (defconstant +lisp-bignum+ "Lorg/armedbear/lisp/Bignum;")
 (defconstant +lisp-single-float-class+ "org/armedbear/lisp/SingleFloat")
@@ -2070,7 +2072,7 @@ the Java object representing SYMBOL can be retrieved."
      ;; fixme *declare-inline*
      (declare-field g +lisp-object+ +field-access-default+)
      (emit 'ldc (pool-string (file-namestring pathname)))
-     (emit-invokestatic "org/armedbear/lisp/AutoloadedFunctionProxy" "loadPreloadedFunction"
+     (emit-invokestatic +lisp-function-proxy-class+ "loadPreloadedFunction"
 			(list +java-string+) +lisp-object+)
      (emit 'putstatic *this-class* g +lisp-object+)
      (setf *static-code* *code*)
