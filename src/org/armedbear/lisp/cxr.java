ca24a4d9091b462a90bc31eba0204465bac76fee
vvoutilainen@1c010e3e-69d0-11dd-93a8-456734b0d56f <vvoutilainen@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2010-02-14 13:32:12 +0000

    Reindentation.

--- src/org/armedbear/lisp/cxr.java
+++ src/org/armedbear/lisp/cxr.java
@@ -35,324 +35,283 @@ package org.armedbear.lisp;
 
 import static org.armedbear.lisp.Lisp.*;
 
-public final class cxr
-{
-  // ### set-car
-  private static final Primitive SET_CAR = new pf_set_car();
-  private static final class pf_set_car extends Primitive {
-      pf_set_car()
-      {
-        super("set-car", PACKAGE_SYS, true);
-      }
-	
-      @Override
-      public LispObject execute(LispObject first, LispObject second)
+public final class cxr {
+    // ### set-car
+    private static final Primitive SET_CAR = new pf_set_car();
+    private static final class pf_set_car extends Primitive {
+        pf_set_car() {
+            super("set-car", PACKAGE_SYS, true);
+        }
 
-      {
-        first.setCar(second);
-        return second;
-      }
+        @Override
+        public LispObject execute(LispObject first, LispObject second)
+
+        {
+            first.setCar(second);
+            return second;
+        }
     };
 
-  // ### set-cdr
-  private static final Primitive SET_CDR = new pf_set_cdr();
-  private static final class pf_set_cdr extends Primitive {
-      pf_set_cdr()
-      {
-        super("set-cdr", PACKAGE_SYS, true);
-      }
-	
-      @Override
-      public LispObject execute(LispObject first, LispObject second)
+    // ### set-cdr
+    private static final Primitive SET_CDR = new pf_set_cdr();
+    private static final class pf_set_cdr extends Primitive {
+        pf_set_cdr() {
+            super("set-cdr", PACKAGE_SYS, true);
+        }
 
-      {
-        first.setCdr(second);
-        return second;
-      }
+        @Override
+        public LispObject execute(LispObject first, LispObject second)
+
+        {
+            first.setCdr(second);
+            return second;
+        }
     };
 
-  // ### car
-  private static final Primitive CAR = new pf_car();
-  private static final class pf_car extends Primitive {
-      pf_car()
-      {
-        super(Symbol.CAR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.car();
-      }
+    // ### car
+    private static final Primitive CAR = new pf_car();
+    private static final class pf_car extends Primitive {
+        pf_car() {
+            super(Symbol.CAR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.car();
+        }
     };
 
-  // ### cdr
-  private static final Primitive CDR = new pf_cdr();
-  private static final class pf_cdr extends Primitive {
-      pf_cdr()
-      {
-        super(Symbol.CDR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.cdr();
-      }
+    // ### cdr
+    private static final Primitive CDR = new pf_cdr();
+    private static final class pf_cdr extends Primitive {
+        pf_cdr() {
+            super(Symbol.CDR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.cdr();
+        }
     };
 
-  // ### caar
-  private static final Primitive CAAR = new pf_caar();
-  private static final class pf_caar extends Primitive {
-      pf_caar()
-      {
-        super(Symbol.CAAR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.car().car();
-      }
+    // ### caar
+    private static final Primitive CAAR = new pf_caar();
+    private static final class pf_caar extends Primitive {
+        pf_caar() {
+            super(Symbol.CAAR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.car().car();
+        }
     };
 
-  // ### cadr
-  private static final Primitive CADR = new pf_cadr();
-  private static final class pf_cadr extends Primitive {
-      pf_cadr()
-      {
-        super(Symbol.CADR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.cadr();
-      }
+    // ### cadr
+    private static final Primitive CADR = new pf_cadr();
+    private static final class pf_cadr extends Primitive {
+        pf_cadr() {
+            super(Symbol.CADR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.cadr();
+        }
     };
 
-  // ### cdar
-  private static final Primitive CDAR = new pf_cdar();
-  private static final class pf_cdar extends Primitive {
-      pf_cdar()
-      {
-        super(Symbol.CDAR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.car().cdr();
-      }
+    // ### cdar
+    private static final Primitive CDAR = new pf_cdar();
+    private static final class pf_cdar extends Primitive {
+        pf_cdar() {
+            super(Symbol.CDAR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.car().cdr();
+        }
     };
 
-  // ### cddr
-  private static final Primitive CDDR = new pf_cddr();
-  private static final class pf_cddr extends Primitive {
-      pf_cddr()
-      {
-        super(Symbol.CDDR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.cdr().cdr();
-      }
+    // ### cddr
+    private static final Primitive CDDR = new pf_cddr();
+    private static final class pf_cddr extends Primitive {
+        pf_cddr() {
+            super(Symbol.CDDR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.cdr().cdr();
+        }
     };
 
-  // ### caddr
-  private static final Primitive CADDR = new pf_caddr();
-  private static final class pf_caddr extends Primitive {
-      pf_caddr()
-      {
-        super(Symbol.CADDR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.caddr();
-      }
+    // ### caddr
+    private static final Primitive CADDR = new pf_caddr();
+    private static final class pf_caddr extends Primitive {
+        pf_caddr() {
+            super(Symbol.CADDR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.caddr();
+        }
     };
 
-  // ### caadr
-  private static final Primitive CAADR = new pf_caadr();
-  private static final class pf_caadr extends Primitive {
-      pf_caadr()
-      {
-        super(Symbol.CAADR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.cdr().car().car();
-      }
+    // ### caadr
+    private static final Primitive CAADR = new pf_caadr();
+    private static final class pf_caadr extends Primitive {
+        pf_caadr() {
+            super(Symbol.CAADR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.cdr().car().car();
+        }
     };
 
-  // ### caaar
-  private static final Primitive CAAAR = new pf_caaar();
-  private static final class pf_caaar extends Primitive {
-      pf_caaar()
-      {
-        super(Symbol.CAAAR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.car().car().car();
-      }
+    // ### caaar
+    private static final Primitive CAAAR = new pf_caaar();
+    private static final class pf_caaar extends Primitive {
+        pf_caaar() {
+            super(Symbol.CAAAR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.car().car().car();
+        }
     };
 
-  // ### cdaar
-  private static final Primitive CDAAR = new pf_cdaar();
-  private static final class pf_cdaar extends Primitive {
-      pf_cdaar()
-      {
-        super(Symbol.CDAAR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.car().car().cdr();
-      }
+    // ### cdaar
+    private static final Primitive CDAAR = new pf_cdaar();
+    private static final class pf_cdaar extends Primitive {
+        pf_cdaar() {
+            super(Symbol.CDAAR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.car().car().cdr();
+        }
     };
 
-  // ### cddar
-  private static final Primitive CDDAR = new pf_cddar();
-  private static final class pf_cddar extends Primitive {
-      pf_cddar()
-      {
-        super(Symbol.CDDAR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.car().cdr().cdr();
-      }
+    // ### cddar
+    private static final Primitive CDDAR = new pf_cddar();
+    private static final class pf_cddar extends Primitive {
+        pf_cddar() {
+            super(Symbol.CDDAR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.car().cdr().cdr();
+        }
     };
 
-  // ### cdddr
-  private static final Primitive CDDDR = new pf_cdddr();
-  private static final class pf_cdddr extends Primitive {
-      pf_cdddr()
-      {
-        super(Symbol.CDDDR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.cdr().cdr().cdr();
-      }
+    // ### cdddr
+    private static final Primitive CDDDR = new pf_cdddr();
+    private static final class pf_cdddr extends Primitive {
+        pf_cdddr() {
+            super(Symbol.CDDDR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.cdr().cdr().cdr();
+        }
     };
 
-  // ### cadar
-  private static final Primitive CADAR = new pf_cadar();
-  private static final class pf_cadar extends Primitive {
-      pf_cadar()
-      {
-        super(Symbol.CADAR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.car().cdr().car();
-      }
+    // ### cadar
+    private static final Primitive CADAR = new pf_cadar();
+    private static final class pf_cadar extends Primitive {
+        pf_cadar() {
+            super(Symbol.CADAR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.car().cdr().car();
+        }
     };
 
-  // ### cdadr
-  private static final Primitive CDADR = new pf_cdadr();
-  private static final class pf_cdadr extends Primitive {
-      pf_cdadr()
-      {
-        super(Symbol.CDADR, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.cdr().car().cdr();
-      }
+    // ### cdadr
+    private static final Primitive CDADR = new pf_cdadr();
+    private static final class pf_cdadr extends Primitive {
+        pf_cdadr() {
+            super(Symbol.CDADR, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.cdr().car().cdr();
+        }
     };
 
-  // ### first
-  private static final Primitive FIRST = new pf_first();
-  private static final class pf_first extends Primitive {
-      pf_first()
-      {
-        super(Symbol.FIRST, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.car();
-      }
+    // ### first
+    private static final Primitive FIRST = new pf_first();
+    private static final class pf_first extends Primitive {
+        pf_first() {
+            super(Symbol.FIRST, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.car();
+        }
     };
 
-  // ### second
-  private static final Primitive SECOND = new pf_second();
-  private static final class pf_second extends Primitive {
-      pf_second()
-      {
-        super(Symbol.SECOND, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.cadr();
-      }
+    // ### second
+    private static final Primitive SECOND = new pf_second();
+    private static final class pf_second extends Primitive {
+        pf_second() {
+            super(Symbol.SECOND, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.cadr();
+        }
     };
 
-  // ### third
-  private static final Primitive THIRD = new pf_third();
-  private static final class pf_third extends Primitive {
-      pf_third()
-      {
-        super(Symbol.THIRD, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.caddr();
-      }
+    // ### third
+    private static final Primitive THIRD = new pf_third();
+    private static final class pf_third extends Primitive {
+        pf_third() {
+            super(Symbol.THIRD, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.caddr();
+        }
     };
 
-  // ### fourth
-  private static final Primitive FOURTH = new pf_fourth();
-  private static final class pf_fourth extends Primitive {
-      pf_fourth()
-      {
-        super(Symbol.FOURTH, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.cdr().cdr().cadr();
-      }
+    // ### fourth
+    private static final Primitive FOURTH = new pf_fourth();
+    private static final class pf_fourth extends Primitive {
+        pf_fourth() {
+            super(Symbol.FOURTH, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.cdr().cdr().cadr();
+        }
     };
 
-  // ### rest
-  private static final Primitive REST = new pf_rest();
-  private static final class pf_rest extends Primitive {
-      pf_rest()
-      {
-        super(Symbol.REST, "list");
-      }
-	
-      @Override
-      public LispObject execute(LispObject arg)
-      {
-        return arg.cdr();
-      }
+    // ### rest
+    private static final Primitive REST = new pf_rest();
+    private static final class pf_rest extends Primitive {
+        pf_rest() {
+            super(Symbol.REST, "list");
+        }
+
+        @Override
+        public LispObject execute(LispObject arg) {
+            return arg.cdr();
+        }
     };
 }
