518225b4be8f224508c85724886f33cc11ad1e58
ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2010-04-12 21:06:03 +0000

    Correct copyright year number; surely the file didn't exist in 1009.

--- src/org/armedbear/lisp/SpecialBindingsMark.java
+++ src/org/armedbear/lisp/SpecialBindingsMark.java
@@ -1,7 +1,7 @@
 /*
  * SpecialBindingsMark.java
  *
- * Copyright (C) 1009 Erik Huelsmann
+ * Copyright (C) 2009 Erik Huelsmann
  * $Id$
  *
  * This program is free software; you can redistribute it and/or
