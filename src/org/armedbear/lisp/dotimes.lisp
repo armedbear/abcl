c9061f136bee7a80507ee80820f3fc83723fa543
mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2011-05-30 13:40:19 +0000

    Fix #144 via correcly naming hints for compiler properties.

--- src/org/armedbear/lisp/dotimes.lisp
+++ src/org/armedbear/lisp/dotimes.lisp
@@ -54,9 +54,9 @@
           (let ((limit (gensym "LIMIT-")))
             ;; Annotations for the compiler.
             (setf (get limit 'dotimes-limit-variable-p) t)
-            (setf (get limit 'dotimes-index-variable-name) index)
+            (setf (get index 'dotimes-index-variable-name) index)
             (setf (get index 'dotimes-index-variable-p) t)
-            (setf (get index 'dotimes-limit-variable-name) limit)
+            (setf (get limit 'dotimes-limit-variable-name) limit)
             `(block nil
                (let ((,var 0)
                      (,limit ,count)
