73099939740e19064141b26f727829bc8deb98e2
mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2011-06-17 10:11:24 +0000

    Implementation strategy:  use the Maven Ant tasks to drive via build.xml.

--- contrib/abcl-asdf/abcl-asdf.lisp
+++ contrib/abcl-asdf/abcl-asdf.lisp
@@ -2,12 +2,17 @@
   (:use :cl)
   (:export #:package))
 
+(defpackage #:mvn
+  (:use :cl)
+  (:export #:satisfy))
+
 (in-package :asdf-jar)
 
 (in-package :asdf)
 (defclass iri (static-class) ())
 
 (defclass mvn (iri) ())
+
 
 ;;; We interpret compilation to ensure that load-op will succeed
 (defmethod perform ((operation compile-op) (component mvn))
@@ -29,4 +34,46 @@
     (:version "1.4.10")))
 
 (defun mvn:satisfy (name &key (version :latest))
-  ;;; XXX either invoke mvn in the same jvm or fork a process)
+  (let ((build.xml (make-temp-file)))
+    (with-open-file (s build.xml :direction :output)
+      (write-string *ant-build-template* s ))
+    (run-program 
+     (format nil "ant -find ~A" build.xml))))
+
+#|
+
+Ant with Maven tasks would add the following
+
+  <artifact:dependencies pathId="abcl.dynamic.classpath">
+    <dependency groupId="junit" artifactId="junit" version="3.8.2"/>
+  </artifact:dependencies>
+|#
+
+(defvar *ant-build-template*
+  (format nil
+  "<?xml version='1.0' encoding='UTF-8'?>
+<project xmlns='antlib:org.apache.tools.ant'
+         name='abcl-mvn-~A' default='install'>
+
+  <artifact:dependencies pathId='abcl.dynamic.classpath'>
+    <dependency groupId='~A' artifactId='~A' version='~A'/>
+  </artifact:dependencies>
+
+  <path id='abcl.jar'> 
+    <pathelement location='/export/home/evenson/work/abcl/dist/abcl.jar'/>
+  </path>
+
+  <target name='install'>
+    <java classname='org.armedbear.lisp.Main'>
+      <classpath refid='abcl.jar'>
+      <classpath refid='abcl.dynamic.classpath'>
+    </java>
+  </target>
+</project>
+" (symbol-name (gensym)) "junit" "junit" "3.8.2"))
+
+
+
+
+  
+
