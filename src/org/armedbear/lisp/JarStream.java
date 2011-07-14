0dcc548b505679ba46fb1a9a804c77c2bf3455a1
ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2011-07-14 19:23:11 +0000

    Set svn:keywords and svn:eol-style properties.

--- src/org/armedbear/lisp/JarStream.java
+++ src/org/armedbear/lisp/JarStream.java
@@ -2,7 +2,7 @@
  * JarStream.java
  *
  * Copyright (C) 2010 Mark Evenson
- * $Id: FileStream.java 12422 2010-02-06 10:52:32Z mevenson $
+ * $Id$
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
--- src/org/armedbear/lisp/WeakReference.java
+++ src/org/armedbear/lisp/WeakReference.java
@@ -2,7 +2,7 @@
  * WeakReference.java
  *
  * Copyright (C) 2011 Erik Huelsmann
- * $Id: JavaStackFrame.java 12288 2009-11-29 22:00:12Z vvoutilainen $
+ * $Id$
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
