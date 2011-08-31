027c9b29b4767676020c559c26020efaed91b126
mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2011-08-31 12:07:26 +0000

    Final metrics for initial run for every tenth commit

--- tools/jar-size/build-metrics.lisp
+++ tools/jar-size/build-metrics.lisp
@@ -1,16 +1,17 @@
-(defun write (&key (file "build-metrics.gnuplot"))
-  (format t "Writing gnuplot file to ~A.~%" file)
-  (with-open-file (s file :direction :output :if-exists :supersede)
-    (format s "# hg-changeset-number svn-revision date abcl.jar-size user-build-time~%")
-    (dolist (line (nreverse (parse)))
-      (format s "~A~%" line))))
+(defun write-data (&key (file "build-metrics.data"))
+  (let ((results (nreverse (parse))))
+    (format t "Writing gnuplot file to ~A.~%" file)
+    (with-open-file (s file :direction :output :if-exists :supersede)
+      (format s "# hg-changeset-number svn-revision date abcl.jar-size user-build-time~%")
+      (dolist (line results)
+        (format s "~A~%" line)))))
 
 (defun parse (&key (file "build-metrics.out"))
   (let (result)
     (format t "Reading raw build metrics from ~A.~%" file)
     (with-open-file (s file)
       (loop
-         (unless (equal (peek-char nil s nil #\-) #\-)
+         (when (eq (peek-char nil s nil 'EOF) 'EOF)
            (return result))
          (let ((record (read-record s)))
            (when (null record)
@@ -39,20 +40,27 @@
                          result)))))))))
         
 (defun read-record (s)
-  (let ((begin (read-line s))
-        result)
-    (unless (string-equal begin "-----")
-      (error "Stream ~a not at beginning of record: ~a" s begin))
-    (loop 
-       (when (equal (peek-char nil s nil #\-) #\-) (return result))
-       (let* ((line (read-line s))
-              (space (search " " line)))
-         (when (numberp space)
-           (let ((key (subseq line 0 space))
-                 (value (subseq line (1+ space))))
-             (when (equal #\: (char key (1- (length key))))
-               (setf key (subseq key 0 (1- (length key)))))
-             (push (cons key value) result)))))
-    result))
+  (let (result)
+    (handler-case
+        (let ((begin (read-line s)))
+          (unless (string-equal begin "-----")
+            (error "Stream ~a not at beginning of record: ~a" s begin))
+          (loop 
+             (when (equal (peek-char nil s) #\-) (return-from read-record result))
+             (let* ((line (read-line s))
+                    (space (search " " line)))
+               (when (numberp space)
+                 (let ((key (subseq line 0 space))
+                       (value (subseq line (1+ space))))
+                   (when (equal #\: (char key (1- (length key))))
+                     (setf key (subseq key 0 (1- (length key)))))
+                   (push (cons key value) result)))))
+          result)
+      (end-of-file () (return-from read-record result)))))
+
+
+
+
+
 
     
