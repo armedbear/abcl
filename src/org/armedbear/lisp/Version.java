91a38bbc301103edf6c32fdd60c805fad628fa21
ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f <ehuelsmann@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2011-09-03 09:36:54 +0000

    Increase trunk version number.

--- src/org/armedbear/lisp/Version.java
+++ src/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "0.27.0-dev";
+  static final String baseVersion = "0.28.0-dev";
   
   static void init() {
     try {
