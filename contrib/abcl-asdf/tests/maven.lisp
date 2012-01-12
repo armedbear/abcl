71dd2aa56df1e2e41cfe55137cd03759c6de02d5
mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2012-01-12 19:38:50 +0000

    The Bear eats its tail.
    Use Maven to locate, retrieve, and cache the latest binary
    of the implementation's jar archive.

--- contrib/abcl-asdf/tests/maven.lisp
+++ contrib/abcl-asdf/tests/maven.lisp
@@ -1,5 +1,8 @@
 #|
 (abcl-asdf:resolve-dependencies "log4j" "log4j")
+
+(abcl-asdf:resolve-dependencies "org.armedbear.lisp" "abcl")
+
 |#
 
 ; TODO figure out what sort of test framework we can hook in.  Probably ABCL-RT
