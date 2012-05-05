a8dad6cb2ff95f7cd0f6db1561c8efd8d089e52a
rschlatte@1c010e3e-69d0-11dd-93a8-456734b0d56f <rschlatte@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2012-05-05 14:05:28 +0000

    Better error reporting involving partially-initialized class metaobjects

--- src/org/armedbear/lisp/print-object.lisp
+++ src/org/armedbear/lisp/print-object.lisp
@@ -52,7 +52,13 @@
   (print-unreadable-object (class stream :identity t)
     (format stream "~S ~S"
             (class-name (class-of class))
-            (class-name class)))
+            ;; Handle partially-initialized class metaobjects
+            ;; gracefully; useful for error reporting.
+            (cond ((not (slot-exists-p class 'name))
+                   "(a class object without name slot)")
+                  ((not (slot-boundp class 'name))
+                   "(a class object with unset name)")
+                  (t (class-name class)))))
   class)
 
 (defmethod print-object ((gf generic-function) stream)
