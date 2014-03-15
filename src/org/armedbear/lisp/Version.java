2903a0ccd48a9ccdc637e04892c6537447f24be1
mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2014-03-15 12:57:23 +0000

    trunk now builds abcl-1.4.0-dev

--- src/org/armedbear/lisp/Version.java
+++ src/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "1.3.0";
+  static final String baseVersion = "1.4.0-dev";
   
   static void init() {
     try {
