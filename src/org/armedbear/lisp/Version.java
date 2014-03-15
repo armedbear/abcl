09f63621716d4b9512e53958486f431c457f8964
mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2014-03-15 12:53:16 +0000

    1.3.0 metadata change before branch.

--- src/org/armedbear/lisp/Version.java
+++ src/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "1.3.0-dev";
+  static final String baseVersion = "1.3.0";
   
   static void init() {
     try {
