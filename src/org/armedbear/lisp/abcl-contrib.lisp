a5c53bdc2e92859d3586442cdc88e9b61cf1fc96
mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2014-03-21 10:26:01 +0000

    Remove trailing whitespace and untabify.

--- src/org/armedbear/lisp/abcl-contrib.lisp
+++ src/org/armedbear/lisp/abcl-contrib.lisp
@@ -2,10 +2,10 @@
 
 (require :asdf)
 
-(defconstant +get-classloader+ 
+(defconstant +get-classloader+
   (java:jmethod "java.lang.Class" "getClassLoader"))
 
-(defun boot-classloader () 
+(defun boot-classloader ()
   (let ((boot-class (java:jclass "org.armedbear.lisp.Main")))
     (java:jcall +get-classloader+ boot-class)))
 
@@ -18,35 +18,35 @@
 (defun named-jar-p (name p)
   (and (pathnamep p)
        (equal (pathname-type p) "jar")
-       (or 
+       (or
         (java:jstatic "matches"
-                      "java.util.regex.Pattern" 
+                      "java.util.regex.Pattern"
                       (concatenate 'string name "(-[0-9]\\.[0-9]\\.[0-9]([+~-].+)?)?")
                       (pathname-name p))
         (java:jstatic "matches"
-                      "java.util.regex.Pattern" 
+                      "java.util.regex.Pattern"
                       (concatenate 'string name "(-[0-9]\\.[0-9]\\.[0-9]\\.[0-9]([+~-]+)?)?")
                       (pathname-name p)))
        (make-pathname :defaults p :name name)))
 
 (defun find-system ()
-  "Find the location of the system.  
+  "Find the location of the system.
 
 Used to determine relative pathname to find 'abcl-contrib.jar'."
-  (or 
-   (ignore-errors 
+  (or
+   (ignore-errors
      (find-system-jar))
    (ignore-errors
-     (some 
-      (lambda (u) 
-        (probe-file (make-pathname 
-                     :defaults  (java:jcall "toString" u)
+     (some
+      (lambda (u)
+        (probe-file (make-pathname
+                     :defaults (java:jcall "toString" u)
                      :name "abcl")))
       (java:jcall "getURLs" (boot-classloader))))
-   (ignore-errors 
+   (ignore-errors
      #p"http://abcl.org/releases/current/abcl.jar")))
 
-(defun find-system-jar () 
+(defun find-system-jar ()
   "Return the pathname of the system jar, one of `abcl.jar` or `abcl-m.n.p.jar` or `abcl-m.n.p[.~-]something.jar`."
     (dolist (loader (java:dump-classpath))
       (let ((abcl-jar (some #'system-jar-p loader)))
@@ -79,7 +79,7 @@ Returns the pathname of the contrib if it can be found."
   (if *abcl-contrib*
       (format verbose "~&Using already initialized value of abcl-contrib:~&'~A'.~%"
               *abcl-contrib*)
-      (progn 
+      (progn
         (setf *abcl-contrib* (find-contrib))
         (format verbose "~&Using probed value of abcl-contrib:~&'~A'.~%"
                 *abcl-contrib*)))
@@ -87,29 +87,21 @@ Returns the pathname of the contrib if it can be found."
 
 (defun find-contrib ()
   "Introspect runtime classpaths to find a loadable ABCL-CONTRIB."
-  (or (ignore-errors 
-		(when (find-system-jar)
-		  (probe-file 
-		   (make-pathname :defaults (find-system-jar)
-						  :name "abcl-contrib"))))
-	  (some 
-	   (lambda (u) 
-		 (probe-file (make-pathname 
-					  :defaults  (java:jcall "toString" u)
-					  :name "abcl-contrib")))
-	   (java:jcall "getURLs" (boot-classloader)))))
+  (or (ignore-errors
+                (when (find-system-jar)
+                  (probe-file
+                   (make-pathname :defaults (find-system-jar)
+                                                  :name "abcl-contrib"))))
+          (some
+           (lambda (u)
+                 (probe-file (make-pathname
+                                          :defaults (java:jcall "toString" u)
+                                          :name "abcl-contrib")))
+           (java:jcall "getURLs" (boot-classloader)))))
 
-(export `(find-system 
+(export `(find-system
           find-contrib
           *abcl-contrib*))
+
 (when (find-and-add-contrib :verbose t)
   (provide :abcl-contrib))
-
-
-
-
-
-
-
-           
-  
