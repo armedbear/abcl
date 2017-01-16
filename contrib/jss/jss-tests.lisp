58abe333932c4df139bf4c2e5c4ab3679adc3223
mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2017-01-16 17:30:22 +0000

    merge tests from tests.lisp into prove tests (Alan Ruttenberg)
    From <https://github.com/armedbear/abcl/pull/25/commits/29cf2f4755eae93e06eafc5c82b9da14cf070ff0>.

--- contrib/jss/jss-tests.lisp
+++ contrib/jss/jss-tests.lisp
@@ -5,10 +5,19 @@
 
 (in-package :jss-test)
 
-(plan 4)
+(plan 6)
 
 (is (read-from-string "#\"{bar}.{foo}\"") '(get-java-field bar foo t))
 (is (read-from-string "#\"q.bar.{foo}\"") '(get-java-field (load-time-value (find-java-class "q.bar")) foo t))
 (is (read-from-string "#\"{bar}.foo\"") '(get-java-field bar "foo" t))
 (is-error (read-from-string "#\".bar.foo\"") 'simple-error)
+;;; http://abcl.org/trac/ticket/205
+(is (with-constant-signature ((substring "substring")) (substring "01234" 2)) "234")
+;;; http://abcl.org/trac/ticket/229
+;;; http://abcl.org/trac/ticket/229 - note: version of test for this ticket was wrong in tests.lisp
+(is (#"toString" (find "size" 
+      (#"getMethods" (find-java-class "java.util.Collections$UnmodifiableMap"))
+	  :test 'string-equal :key #"getName"))
+    (#"toString" (java::jmethod "java.util.Collections$UnmodifiableMap" "size" )))
+
 (finalize)
