3e9865a5b9d58e5a12a48c41d964eefeee0ab083
mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2017-06-08 18:29:39 +0000

    Fix test on Java 6
    (Olof-Joachim Frahm)
    
    From <https://github.com/armedbear/abcl/pull/49/commits/358502d258722d008f37ed1b49eaf4537ca93a6e>.
    
    Merges <https://github.com/armedbear/abcl/pull/49>.

--- test/lisp/abcl/java.lisp
+++ test/lisp/abcl/java.lisp
@@ -1,17 +1,17 @@
 (in-package #:abcl.test.lisp)
 
 (deftest java.truth.1
-  (let ((java.lang.boolean.compare 
-         (java:jmethod "java.lang.Boolean" "compare" "boolean" "boolean"))
+  (let ((java.lang.boolean.compare-to
+         (java:jmethod "java.lang.Boolean" "compareTo" "java.lang.Boolean"))
         (java.lang.boolean.equals 
          (java:jmethod "java.lang.Boolean" "equals" "java.lang.Object")))
     (values 
-     (java:jstatic java.lang.Boolean.compare "java.lang.Boolean" java:+true+ t)
-     (java:jstatic java.lang.Boolean.compare "java.lang.Boolean" java:+false+ nil)
+     (java:jcall java.lang.Boolean.compare-to java:+true+ t)
+     (java:jcall java.lang.Boolean.compare-to java:+false+ nil)
      (java:jcall java.lang.Boolean.equals java:+true+ t)
      (java:jcall java.lang.Boolean.equals java:+false+ nil)
-     (java:jstatic java.lang.Boolean.compare "java.lang.Boolean" java:+false+ t)
-     (java:jstatic java.lang.Boolean.compare "java.lang.Boolean" java:+false+ t)
+     (java:jcall java.lang.Boolean.compare-to java:+false+ t)
+     (java:jcall java.lang.Boolean.compare-to java:+false+ t)
      (java:jcall java.lang.Boolean.equals java:+true+ nil)
      (java:jcall java.lang.Boolean.equals java:+false+ t)))
   0 0 t t -1 -1 nil nil)
