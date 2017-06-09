f7db79c541765fd5b0834d03fe42676ef4c16e8e
mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2017-06-09 08:50:45 +0000

    Javaparser, preliminary to hook in jss (Alan Ruttenberg)
    From <https://github.com/armedbear/abcl/pull/52/commits/291289e0c6da1c08b5e2cf53c41eb3bb8aa48ffc>
    
    Part of merge <https://github.com/armedbear/abcl/pull/52>.

---
+++ contrib/jss/javaparser.lisp
@@ -0,0 +1,66 @@
+(in-package :jss)
+
+(defvar *class-to-last-component* (make-hash-table :test 'equalp))
+
+(defclass javaparser () ())
+
+(defmacro def-java-read (ast-class class fields &body body)
+  (let ((jclass (find-java-class (concatenate 'string "com.github.javaparser.ast.expr." (string ast-class)))))
+    `(progn
+       (setf (gethash ,jclass *class-to-last-component*) ',ast-class)
+       (defmethod ,ast-class ((obj ,class) node &optional
+			      ,@(loop for field in fields
+				      collect `(,(intern (string-upcase field)) (get-java-field node ,field t))))
+	   ,@body))))
+
+(defvar *object-for-this* (new 'lang.object))
+
+(defmethod get-optional ((r javaparser) node)
+  (if (equal node (load-time-value (#"empty"  'java.util.Optional ))) nil (#"get" node)))
+
+(defmethod process-node ((r javaparser) node)
+  (when (jinstance-of-p  node "java.util.Optional")
+    (setq node (get-optional r node)))
+  (when (null node)
+    (return-from process-node nil))
+  (if (java-object-p node)
+      (funcall (gethash (jobject-class node) *class-to-last-component*)  r node)
+      node))
+
+(defmethod read-java-expression ((r javaparser) expression)
+  `(let ((this *object-for-this*))
+     ,(process-node r (#"parseExpression" 'javaparser expression))))
+
+(def-java-read LongLiteralExpr javaparser ()
+  (read-from-string (#"replaceFirst" (#"getValue" node) "L" "")))
+
+(def-java-read BooleanLiteralExpr javaparser ()
+  (if (equal (#"getValue" node) "true") t nil))
+
+(def-java-read IntegerLiteralExpr javaparser nil
+ (parse-integer (#"getValue" node)))
+
+(def-java-read DoubleLiteralExpr javaparser nil
+  (let ((raw (#"getValue" node)))
+    (setq raw (#"replaceAll" raw "_" ""))
+    (if (#"matches" raw ".*[dD]$")
+	(read-from-string (#"replaceFirst" (subseq raw 0 (1- (length raw))) "e" "d"))
+	(if (#"matches" raw ".*[fF]$")
+	    (read-from-string (subseq raw 0 (1- (length raw))))
+	    (read-from-string raw)))))
+
+(def-java-read CharLiteralExpr javaparser nil
+  (#"getValue" node))
+
+(def-java-read StringLiteralExpr javaparser nil
+  (#"getValue" node))
+
+(def-java-read NullLiteralExpr javaparser nil
+  +null+)
+
+(def-java-read SimpleName javaparser ()
+  (let ((symbol (intern (#"getIdentifier" node))))
+    symbol))
+
+
+
