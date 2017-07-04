39c23849c36e295a78ea82b85f3df730dfec5a66
mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f <mevenson@1c010e3e-69d0-11dd-93a8-456734b0d56f>
2017-07-04 10:51:23 +0000

    Make JVM class names more readable (Olof-Joachim Frahm)
    From <https://github.com/Ferada/abcl/commit/a9fdcd7f5b3ae9645712af8a7d4b7f43c7618ac3>.

--- src/org/armedbear/lisp/jvm-class-file.lisp
+++ src/org/armedbear/lisp/jvm-class-file.lisp
@@ -105,7 +105,8 @@ representation to use.
 |#
 
 (defstruct (jvm-class-name (:conc-name class-)
-                           (:constructor %make-jvm-class-name))
+                           (:constructor %make-jvm-class-name)
+                           (:print-object %print-jvm-class-name))
   "Used for class identification.
 
 The caller should instantiate only one `class-name' per class, as they are
@@ -119,6 +120,10 @@ This class is used to abstract from the difference."
   ;; keeping a reference to the associated array class allows class
   ;; name comparisons to be EQ: all classes should exist only once,
   )
+
+(defun %print-jvm-class-name (name stream)
+  (print-unreadable-object (name stream :type t)
+    (write-string (class-name-internal name) stream)))
 
 (defun make-jvm-class-name (name)
   "Creates a `class-name' structure for the class or interface `name'.
