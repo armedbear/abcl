f21ede8ffd98ee716c27bfb3dc227363333d7f2e
Alan Ruttenberg <alanruttenberg@gmail.com>
2019-12-17 12:24:07 -0500

    Fix precompiler-setq to signal simple-error vs simple-program-error, since latter doesn't exist

--- src/org/armedbear/lisp/precompiler.lisp
+++ src/org/armedbear/lisp/precompiler.lisp
@@ -543,7 +543,7 @@
   (let* ((args (cdr form))
          (len (length args)))
     (when (oddp len)
-      (error 'simple-program-error
+      (error 'simple-error
              :format-control "Odd number of arguments to SETQ."))
     (if (= len 2)
         (let* ((sym (%car args))
