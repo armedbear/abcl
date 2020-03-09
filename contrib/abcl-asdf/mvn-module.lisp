6e007e5de301c72e2d362bed8b35896e9beddc28
Mark <evenson.not.org@gmail.com>
2020-03-09 20:19:47 +0100

    abcl-asdf: use WITH-AETHER to ensure things are loaded
    Unfortunately, since we can't easily "unload" jar files from the
    classpath, it isn't easy to make the WITH-AETHER macro work with
    differing versions of Aether.  Still, it's a little cleaner to use a
    WITH-AETHER form to ensure that the Maven initialization has been done
    correctly.
    
    TODO: ensure that WITH-AETHER invoked with a bogus Maven Aether
    library location doesn't bork the system.

--- contrib/abcl-asdf/mvn-module.lisp
+++ contrib/abcl-asdf/mvn-module.lisp
@@ -25,39 +25,39 @@
  first-is-root: If the first dependency should include optional
    dependencies, set this to t. Usually not.
  "
-  (let (aether)
-    (with-aether (aether)
-      (let ((collect-request (java:jnew (jss:find-java-class "CollectRequest")))
-            (exclusions-collection (jss:new 'java.util.HashSet))
-            (compile-scope (java:jfield (jss:find-java-class "JavaScopes") "COMPILE")))
-	(#"addRepository" collect-request (ensure-remote-repository))
-        (loop for e  in exclusions
-           for (groupid artifactid) = (abcl-build:split-string e #\:)
-           ;; If i have scope be compile-scope it doesn't get excluded!!
-           for exclusion = (jss:new 'aether.graph.Exclusion groupid artifactid "" "jar")
-           do (#"add" exclusions-collection exclusion))
-        (loop for a in dependencies
-           for artifact = (make-artifact (#"replaceAll" a "/" ":"))
-           for dependency = (jss:new 'aether.graph.Dependency artifact compile-scope)
-           do  
-           ;; setExclusions returns a new dependency. We have to use
-           ;; that. That passed dependency i not modified!
-           ;; http://grepcode.com/file/repo1.maven.org/maven2/org.eclipse.aether/aether-api/1.0.2.v0150114/org/eclipse/aether/graph/Dependency.java#Dependency.getOptional%28%29
+  (with-aether (nil)
+    (let ((collect-request (java:jnew (jss:find-java-class "CollectRequest")))
+          (exclusions-collection (jss:new 'java.util.HashSet))
+          (compile-scope (java:jfield (jss:find-java-class "JavaScopes") "COMPILE")))
+      (#"addRepository" collect-request (ensure-remote-repository))
+      (loop for e  in exclusions
+         for (groupid artifactid) = (abcl-build:split-string e #\:)
+         ;; If i have scope be compile-scope it doesn't get excluded!!
+         for exclusion = (jss:new 'aether.graph.Exclusion groupid artifactid "" "jar")
+         do (#"add" exclusions-collection exclusion))
+      (loop for a in dependencies
+         for artifact = (make-artifact (#"replaceAll" a "/" ":"))
+         for dependency = (jss:new 'aether.graph.Dependency artifact compile-scope)
+         do  
+         ;; setExclusions returns a new dependency. We have to use
+         ;; that. That passed dependency i not modified!
+         ;; http://grepcode.com/file/repo1.maven.org/maven2/org.eclipse.aether/aether-api/1.0.2.v0150114/org/eclipse/aether/graph/Dependency.java#Dependency.getOptional%28%29
            ;; Nice of them to clearly document that :-/
-	     (setq dependency (#"setExclusions" dependency exclusions-collection))
-	     (if first-is-root
-		 (#"setRoot" collect-request dependency)
-		 (#"addDependency" collect-request dependency))
-	     (setq first-is-root nil))
-        (loop for a in managed-dependencies
-           for artifact = (make-artifact (#"replaceAll" a "/" ":"))
-           for dependency = (jss:new 'aether.graph.Dependency artifact compile-scope)
-           do (setq dependency (#"setExclusions" dependency exclusions-collection))
-	     (#"addManagedDependency" collect-request dependency))
-        (let ((dependencies (#"collectDependencies" (ensure-repository-system) (ensure-session) collect-request))
-              (nodelist-generator (jss:new 'PreorderNodeListGenerator))
-              (dependency-request (jss:new 'DependencyRequest)))
-          (#"setRoot" dependency-request (#"getRoot" dependencies))
-          (#"resolveDependencies" (ensure-repository-system) (ensure-session) dependency-request)
-          (#"accept" (#"getRoot" dependencies) nodelist-generator)
-          (abcl-build:split-string (#"getClassPath" nodelist-generator) #\:))))))
+	   (setq dependency (#"setExclusions" dependency exclusions-collection))
+	   (if first-is-root
+	       (#"setRoot" collect-request dependency)
+	       (#"addDependency" collect-request dependency))
+	   (setq first-is-root nil))
+      (loop for a in managed-dependencies
+         for artifact = (make-artifact (#"replaceAll" a "/" ":"))
+         for dependency = (jss:new 'aether.graph.Dependency artifact compile-scope)
+         do (setq dependency (#"setExclusions" dependency exclusions-collection))
+	   (#"addManagedDependency" collect-request dependency))
+      (let ((dependencies (#"collectDependencies" (ensure-repository-system) (ensure-session) collect-request))
+            (nodelist-generator (jss:new 'PreorderNodeListGenerator))
+            (dependency-request (jss:new 'DependencyRequest)))
+        (#"setRoot" dependency-request (#"getRoot" dependencies))
+        (#"resolveDependencies" (ensure-repository-system) (ensure-session) dependency-request)
+        (#"accept" (#"getRoot" dependencies) nodelist-generator)
+        (abcl-build:split-string (#"getClassPath" nodelist-generator) #\:)))))
+
