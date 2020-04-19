36fc298f7e4cbff2781dcc2a1179ab43eadd51d0
Mark <evenson.not.org@gmail.com>
2020-04-19 19:15:09 +0200

    ci: start setting more specific runtime options
    Use the Z GC on openjdk11; the G1 GC on openjdk8.

---
+++ ci/create-abcl-properties.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+jdk=$1
+if [[ -z $jdk ]]; then
+    jdk=openjdk8
+fi
+
+case $jdk in
+    openjdk8)
+        options="-XX:+UseG1GC -XX:+AggressiveOpts -XX:CompileThreshold=10"
+        ;;
+    openjdk11)
+        options="-XX:+UseZGC"
+        ;;
+esac
+
+cat abcl.properties.in | awk -F = -v options="$options" '/^java.options/ {print $0 " " options; next}; {print $0}' > abcl.properties
+
