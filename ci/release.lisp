97efb62795303ceccc66454f6da35426f7ae0b79
Mark <evenson.not.org@gmail.com>
2020-04-26 10:50:12 +0200

    ci: fix self-hosted release script

--- ci/release.lisp
+++ ci/release.lisp
@@ -1,14 +1,14 @@
 (require :asdf)
-(asdf:load-system :abcl-contrib)
-(asdf:load-system :abcl)
+(require :abcl-contrib)
 
 (if (not (asdf:find-system :abcl-build))
-    (warn "Can't find <urn:abcl.org/contrib/abcl-build> in ASDF.")
+    (warn "Failed to find <urn:abcl.org/contrib/abcl-build> in ASDF to build release.")
     (let (ant)
-      (asdf:load-system :abcl-build)
+      (asdf:make :abcl-build)
       (abcl-build:with-ensured-ant (ant)
         (abcl/build:ant/call "build.xml"
                              '("abcl.release" "abcl.wrapper")))))
+
 
 
     
