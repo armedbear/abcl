f12c5f8f92020d00a959a2a4971d02237500762a
Mark <evenson.not.org@gmail.com>
2020-04-26 12:34:38 +0200

    ci: another attempt at getting the self-hosted release correct

--- ci/release.lisp
+++ ci/release.lisp
@@ -3,11 +3,13 @@
 
 (if (not (asdf:find-system :abcl-build))
     (warn "Failed to find <urn:abcl.org/contrib/abcl-build> in ASDF to build release.")
-    (let (ant)
-      (asdf:make :abcl-build)
-      (abcl-build:with-ensured-ant (ant)
-        (abcl/build:ant/call "build.xml"
-                             '("abcl.release" "abcl.wrapper")))))
+    (asdf:make :abcl-build))
+
+(let (ant)
+  (abcl-build:with-ensured-ant (ant)
+    (abcl-build:ant/call "build.xml"
+                         '("abcl.release" "abcl.wrapper"))))))
+
 
 
 
