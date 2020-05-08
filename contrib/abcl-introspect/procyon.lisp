4dd590c2c5380e24891f250487aeeecb2cbe4517
Mark <evenson.not.org@gmail.com>
2020-05-08 13:02:27 +0200

    Start blocking out javap and procyon

--- contrib/abcl-introspect/procyon.lisp
+++ contrib/abcl-introspect/procyon.lisp
@@ -4,5 +4,26 @@
     (:export
      #:disassemble-class-bytes))
 
+#|
+  
+  <https://bitbucket.org/mstrobel/procyon/wiki/Decompiler%20API> 
+
+final DecompilerSettings settings = DecompilerSettings.javaDefaults();
+
+try (final FileOutputStream stream = new FileOutputStream("path/to/file");
+     final OutputStreamWriter writer = new OutputStreamWriter(stream)) {
+
+    Decompiler.decompile(
+        "java/lang/String",
+        new PlainTextOutput(writer),
+        settings
+    );
+}
+catch (final IOException e) {
+    // handle error
+}
+
+|# 
+
 (defun disassemble-class-bytes (object)
   (error "Unimplemented use of procyon dissassembler."))
