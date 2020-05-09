36f1bb5b81cdad0998bd91fc44ad997b6e0fa81d
Mark <evenson.not.org@gmail.com>
2020-05-09 22:37:55 +0200

    cl: get medieval about testings depdendencies for abcl-introspect

--- ci/test-abcl-introspect.lisp
+++ ci/test-abcl-introspect.lisp
@@ -1,2 +1,6 @@
 (ql:quickload :abcl-introspect)
+;;; shouldn't be necessary…
+(ql:quickload :prove)
+(ql:quickload :prove-asdf)
+(ql:quickload :abcl-introspect-tests)
 (asdf:test-system :abcl-introspect)
