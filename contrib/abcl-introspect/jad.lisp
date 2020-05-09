97280c3752ba48af4d2cf10935a4e3f9df3008b7
Mark <evenson.not.org@gmail.com>
2020-05-09 15:33:57 +0200

    abcl-introspect: work on introspecting and/or installing jad
    Basic introspection and retrieval of resources in place.  TODO: add
    correct URIs for introspected operation.  Current URI is not available
    (lot of unexpected escaping going on).

--- contrib/abcl-introspect/jad.lisp
+++ contrib/abcl-introspect/jad.lisp
@@ -17,21 +17,28 @@
    ((uiop/os:os-macosx-p)
     "http://www.javadecompilers.com/jad/Jad%201.5.8g%20for%20Mac%20OS%20X%2010.4.6%20on%20Intel%20platform.zip")))
 
-(defvar *working-jad-path* nil)
+(defvar *working-jad-executable* nil)
   
 (defun ensure-jad ()
-  (if (null *working-jad-path*)
-      (let ((jad-path (abcl-build:introspect-path-for "jad")))
-        (if (and jad-path
-                 (handler-case
-                     (uiop:run-program jad-path)
-                   (uiop/run-program:subprocess-error (e) nil)))
-            (setf *working-jad-path* jad-path))
-        (progn
-          (abcl-build:xdg/install (introspect-jad-uri) :type :unzip)
-          (setf *working-jad-path* nil)))
-      *working-jad-path*))
-
+  (flet
+      ((install-jad-returning-path (uri)
+         (abcl-build:xdg/install (pathname uri) :type :unzip))
+       (working-jad-p (jad-path)
+         (handler-case
+             (uiop:run-program jad-path)
+           (uiop/run-program:subprocess-error (e) nil))))
+      (if (null *working-jad-executable*)
+          (let ((jad-path (abcl-build:introspect-path-for "jad")))
+            (if (and jad-path
+                     (working-jad-p jad-path))
+                (setf *working-jad-executable* jad-path)
+                (progn
+                  (install-jad-returning-path (introspect-jad-uri))
+                  (setf *working-jad-executable* jad-path))))
+          (unless (working-jad-p)
+            (setf *working-jad-executable*
+                  (install-jad-returning-path (introspect-jad-uri)))))))
+          
 (defun disassemble-class-bytes (object)
   (ensure-jad)
   (let ((sys:::*disassembler*
