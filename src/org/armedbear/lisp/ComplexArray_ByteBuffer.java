7a8cfe0f3a10db592ff9d324237bf403377a1124
Mark <evenson.not.org@gmail.com>
2020-05-21 15:07:38 +0200

    Somehow the Complex ByteBuffer types compile
    But as the Hammer said to the Bear, "don't truss it".

--- src/org/armedbear/lisp/ComplexArray_ByteBuffer.java
+++ src/org/armedbear/lisp/ComplexArray_ByteBuffer.java
@@ -1,8 +1,7 @@
 /*
- * ComplexArray_UnsignedByte8.java
+ * ComplexArray_ByteBuffer.java
  *
- * Copyright (C) 2003-2005 Peter Graves
- * $Id$
+ * Copyright (C) 2020 easye
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -37,270 +36,286 @@ import static org.armedbear.lisp.Lisp.*;
 
 import java.nio.ByteBuffer;
 
-public final class ComplexArray_ByteBuffer extends AbstractArray
+public final class ComplexArray_ByteBuffer
+  extends AbstractArray
 {
-    private final int[] dimv;
-    private int totalSize;
+  private final int[] dimv;
+  private int totalSize;
+  
+  // For non-displaced arrays.
+  //    private byte[] data;
+  ByteBuffer data; 
 
-    // For non-displaced arrays.
-    private byte[] data;
+  // For displaced arrays.
+  private AbstractArray array;
+  private int displacement;
 
-    // For displaced arrays.
-    private AbstractArray array;
-    private int displacement;
+  public ComplexArray_ByteBuffer(int[] dimv) {
+    this.dimv = dimv;
+    totalSize = computeTotalSize(dimv);
+    data = ByteBuffer.allocate(totalSize);
+  }
 
-    public ComplexArray_ByteBuffer(int[] dimv)
-    {
-        this.dimv = dimv;
-        totalSize = computeTotalSize(dimv);
-        data = new byte[totalSize];
+  public ComplexArray_ByteBuffer(int[] dimv, LispObject initialContents)
+
+  {
+    this.dimv = dimv;
+    final int rank = dimv.length;
+    LispObject rest = initialContents;
+    for (int i = 0; i < rank; i++) {
+      dimv[i] = rest.length();
+      rest = rest.elt(0);
     }
+    totalSize = computeTotalSize(dimv);
+    data = ByteBuffer.allocate(totalSize);
+    setInitialContents(0, dimv, initialContents, 0);
+  }
 
-    public ComplexArray_ByteBuffer(int[] dimv, LispObject initialContents)
+  public ComplexArray_ByteBuffer(int[] dimv, AbstractArray array, int displacement)
+  {
+    this.dimv = dimv;
+    this.array = array;
+    this.displacement = displacement;
+    totalSize = computeTotalSize(dimv);
+  }
 
-    {
-        this.dimv = dimv;
-        final int rank = dimv.length;
-        LispObject rest = initialContents;
-        for (int i = 0; i < rank; i++) {
-            dimv[i] = rest.length();
-            rest = rest.elt(0);
+  private int setInitialContents(int axis, int[] dims, LispObject contents,
+                                 int index)
+
+  {
+    if (dims.length == 0) {
+      try {
+        data.put(index, coerceLispObjectToJavaByte(contents));
+      }
+      catch (ArrayIndexOutOfBoundsException e) {
+        error(new LispError("Bad initial contents for array."));
+        return -1;
+      }
+      ++index;
+    } else {
+      int dim = dims[0];
+      if (dim != contents.length()) {
+        error(new LispError("Bad initial contents for array."));
+        return -1;
+      }
+      int[] newDims = new int[dims.length-1];
+      for (int i = 1; i < dims.length; i++)
+        newDims[i-1] = dims[i];
+      if (contents.listp()) {
+        for (int i = contents.length();i-- > 0;) {
+          LispObject content = contents.car();
+          index =
+            setInitialContents(axis + 1, newDims, content, index);
+          contents = contents.cdr();
         }
-        totalSize = computeTotalSize(dimv);
-        data = new byte[totalSize];
-        setInitialContents(0, dimv, initialContents, 0);
-    }
-
-    public ComplexArray_ByteBuffer(int[] dimv, AbstractArray array, int displacement)
-    {
-        this.dimv = dimv;
-        this.array = array;
-        this.displacement = displacement;
-        totalSize = computeTotalSize(dimv);
-    }
-
-    private int setInitialContents(int axis, int[] dims, LispObject contents,
-                                   int index)
-
-    {
-        if (dims.length == 0) {
-            try {
-                data[index] = coerceLispObjectToJavaByte(contents);
-            }
-            catch (ArrayIndexOutOfBoundsException e) {
-                error(new LispError("Bad initial contents for array."));
-                return -1;
-            }
-            ++index;
-        } else {
-            int dim = dims[0];
-            if (dim != contents.length()) {
-                error(new LispError("Bad initial contents for array."));
-                return -1;
-            }
-            int[] newDims = new int[dims.length-1];
-            for (int i = 1; i < dims.length; i++)
-                newDims[i-1] = dims[i];
-            if (contents.listp()) {
-                for (int i = contents.length();i-- > 0;) {
-                    LispObject content = contents.car();
-                    index =
-                        setInitialContents(axis + 1, newDims, content, index);
-                    contents = contents.cdr();
-                }
-            } else {
-                AbstractVector v = checkVector(contents);
-                final int length = v.length();
-                for (int i = 0; i < length; i++) {
-                    LispObject content = v.AREF(i);
-                    index =
-                        setInitialContents(axis + 1, newDims, content, index);
-                }
-            }
+      } else {
+        AbstractVector v = checkVector(contents);
+        final int length = v.length();
+        for (int i = 0; i < length; i++) {
+          LispObject content = v.AREF(i);
+          index =
+            setInitialContents(axis + 1, newDims, content, index);
         }
-        return index;
+      }
     }
+    return index;
+  }
 
-    @Override
-    public LispObject typeOf()
-    {
-        return list(Symbol.ARRAY, UNSIGNED_BYTE_8, getDimensions());
+  @Override
+  public LispObject typeOf()
+  {
+    return list(Symbol.ARRAY, UNSIGNED_BYTE_8, getDimensions());
+  }
+
+  @Override
+  public LispObject classOf()
+  {
+    return BuiltInClass.ARRAY;
+  }
+
+  @Override
+  public int getRank()
+  {
+    return dimv.length;
+  }
+
+  @Override
+  public LispObject getDimensions()
+  {
+    LispObject result = NIL;
+    for (int i = dimv.length; i-- > 0;)
+      result = new Cons(Fixnum.getInstance(dimv[i]), result);
+    return result;
+  }
+
+  @Override
+  public int getDimension(int n)
+  {
+    try {
+      return dimv[n];
     }
-
-    @Override
-    public LispObject classOf()
-    {
-        return BuiltInClass.ARRAY;
+    catch (ArrayIndexOutOfBoundsException e) {
+      error(new TypeError("Bad array dimension " + n + "."));
+      return -1;
     }
+  }
 
-    @Override
-    public int getRank()
-    {
-        return dimv.length;
+  @Override
+  public LispObject getElementType()
+  {
+    return UNSIGNED_BYTE_8;
+  }
+
+  @Override
+  public int getTotalSize()
+  {
+    return totalSize;
+  }
+
+  @Override
+  public LispObject arrayDisplacement()
+  {
+    LispObject value1, value2;
+    if (array != null) {
+      value1 = array;
+      value2 = Fixnum.getInstance(displacement);
+    } else {
+      value1 = NIL;
+      value2 = Fixnum.ZERO;
     }
+    return LispThread.currentThread().setValues(value1, value2);
+  }
 
-    @Override
-    public LispObject getDimensions()
-    {
-        LispObject result = NIL;
-        for (int i = dimv.length; i-- > 0;)
-            result = new Cons(Fixnum.getInstance(dimv[i]), result);
-        return result;
+  @Override
+  public LispObject AREF(int index)
+  {
+    if (data != null) {
+      try {
+        return coerceJavaByteToLispObject(data.get(index));
+      }
+      catch (ArrayIndexOutOfBoundsException e) {
+        return error(new TypeError("Bad row major index " + index + "."));
+      }
+    } else
+      return array.AREF(index + displacement);
+  }
+
+  @Override
+  public void aset(int index, LispObject newValue)
+  {
+    if (data != null) {
+      try {
+        data.put(index, coerceLispObjectToJavaByte(newValue));
+      }
+      catch (ArrayIndexOutOfBoundsException e) {
+        error(new TypeError("Bad row major index " + index + "."));
+      }
+    } else
+      array.aset(index + displacement, newValue);
+  }
+
+  @Override
+  public void fill(LispObject obj)
+  {
+    if (!(obj instanceof Fixnum)) {
+      type_error(obj, Symbol.FIXNUM);
+      // Not reached.
+      return;
     }
+    int n = ((Fixnum) obj).value;
+    if (n < 0 || n > 255) {
+      type_error(obj, UNSIGNED_BYTE_8);
+      // Not reached.
+      return;
+    }
+    if (data != null) {
+      for (int i = data.limit(); i-- > 0;)
+        data.put(i, (byte) n); // FIXME Faster!!
+    } else {
+      for (int i = totalSize; i-- > 0;)
+        aset(i, obj);
+    }
+  }
 
-    @Override
-    public int getDimension(int n)
-    {
-        try {
-            return dimv[n];
+  @Override
+  public String printObject()
+  {
+    if (Symbol.PRINT_READABLY.symbolValue() != NIL) {
+      error(new PrintNotReadable(list(Keyword.OBJECT, this)));
+      // Not reached.
+      return null;
+    }
+    return printObject(dimv);
+  }
+
+  int arrayTotalSize(int[] dims) {
+    int result = 0;
+    for (int dim : dims) {
+      result += dim;
+    }
+    return result;
+  }
+
+  // FIXME move me to someplace more general
+  public static void fill(ByteBuffer buffer, byte value) {
+    for (int i = 0; i < buffer.limit(); i++) {
+      buffer.put(value);
+    }
+  }
+
+  @Override
+  public AbstractArray adjustArray(int[] dims,
+                                   LispObject initialElement,
+                                   LispObject initialContents)
+  {
+    if (isAdjustable()) {
+      if (initialContents != null)
+        setInitialContents(0, dims, initialContents, 0);
+      else {
+        //### FIXME Take the easy way out: we don't want to reorganize
+        // all of the array code yet
+        //        SimpleArray_ByteBuffer newBuffer = new SimpleArray_ByteBuffer(dims);
+        ByteBuffer newBuffer = ByteBuffer.allocate(computeTotalSize(dims));
+        if (initialElement != null) {
+          fill(newBuffer, coerceLispObjectToJavaByte(initialElement));           
         }
-        catch (ArrayIndexOutOfBoundsException e) {
-            error(new TypeError("Bad array dimension " + n + "."));
-            return -1;
-        }
+        this.data = newBuffer;
+
+        for (int i = 0; i < dims.length; i++)
+          dimv[i] = dims[i];
+      }
+      return this;
+    } else {
+      if (initialContents != null)
+        return new ComplexArray_ByteBuffer(dims, initialContents);
+      else {
+        ComplexArray_ByteBuffer newArray = new ComplexArray_ByteBuffer(dims);
+        if (initialElement != null)
+          newArray.fill(initialElement);
+        return newArray;
+      }
     }
+  }
 
-    @Override
-    public LispObject getElementType()
-    {
-        return UNSIGNED_BYTE_8;
+  @Override
+  public AbstractArray adjustArray(int[] dims,
+                                   AbstractArray displacedTo,
+                                   int displacement)
+  {
+    if (isAdjustable()) {
+      for (int i = 0; i < dims.length; i++)
+        dimv[i] = dims[i];
+
+      this.data = null;
+      this.array = displacedTo;
+      this.displacement = displacement;
+      this.totalSize = computeTotalSize(dims);
+
+      return this;
+    } else {
+      ComplexArray_ByteBuffer a = new ComplexArray_ByteBuffer(dims, displacedTo, displacement);
+
+      return a;
     }
-
-    @Override
-    public int getTotalSize()
-    {
-        return totalSize;
-    }
-
-    @Override
-    public LispObject arrayDisplacement()
-    {
-        LispObject value1, value2;
-        if (array != null) {
-            value1 = array;
-            value2 = Fixnum.getInstance(displacement);
-        } else {
-            value1 = NIL;
-            value2 = Fixnum.ZERO;
-        }
-        return LispThread.currentThread().setValues(value1, value2);
-    }
-
-    @Override
-    public LispObject AREF(int index)
-    {
-        if (data != null) {
-            try {
-                return coerceJavaByteToLispObject(data[index]);
-            }
-            catch (ArrayIndexOutOfBoundsException e) {
-                return error(new TypeError("Bad row major index " + index + "."));
-            }
-        } else
-            return array.AREF(index + displacement);
-    }
-
-    @Override
-    public void aset(int index, LispObject newValue)
-    {
-        if (data != null) {
-            try {
-                data[index] = coerceLispObjectToJavaByte(newValue);
-            }
-            catch (ArrayIndexOutOfBoundsException e) {
-                error(new TypeError("Bad row major index " + index + "."));
-            }
-        } else
-            array.aset(index + displacement, newValue);
-    }
-
-    @Override
-    public void fill(LispObject obj)
-    {
-        if (!(obj instanceof Fixnum)) {
-            type_error(obj, Symbol.FIXNUM);
-            // Not reached.
-            return;
-        }
-        int n = ((Fixnum) obj).value;
-        if (n < 0 || n > 255) {
-            type_error(obj, UNSIGNED_BYTE_8);
-            // Not reached.
-            return;
-        }
-        if (data != null) {
-            for (int i = data.length; i-- > 0;)
-                data[i] = (byte) n;
-        } else {
-            for (int i = totalSize; i-- > 0;)
-                aset(i, obj);
-        }
-    }
-
-    @Override
-    public String printObject()
-    {
-        if (Symbol.PRINT_READABLY.symbolValue() != NIL) {
-            error(new PrintNotReadable(list(Keyword.OBJECT, this)));
-            // Not reached.
-            return null;
-        }
-        return printObject(dimv);
-    }
-
-
-    @Override
-    public AbstractArray adjustArray(int[] dims,
-                                              LispObject initialElement,
-                                              LispObject initialContents)
-            {
-        if (isAdjustable()) {
-            if (initialContents != null)
-                setInitialContents(0, dims, initialContents, 0);
-            else {
-                //### FIXME Take the easy way out: we don't want to reorganize
-                // all of the array code yet
-                SimpleArray_UnsignedByte8 tempArray = new SimpleArray_UnsignedByte8(dims);
-                if (initialElement != null)
-                    tempArray.fill(initialElement);
-                SimpleArray_UnsignedByte8.copyArray(this, tempArray);
-                this.data = tempArray.data;
-
-                for (int i = 0; i < dims.length; i++)
-                    dimv[i] = dims[i];
-            }
-            return this;
-        } else {
-            if (initialContents != null)
-                return new ComplexArray_ByteBuffer(dims, initialContents);
-            else {
-                ComplexArray_ByteBuffer newArray = new ComplexArray_ByteBuffer(dims);
-                if (initialElement != null)
-                    newArray.fill(initialElement);
-                return newArray;
-            }
-        }
-    }
-
-    @Override
-    public AbstractArray adjustArray(int[] dims,
-                                              AbstractArray displacedTo,
-                                              int displacement)
-            {
-        if (isAdjustable()) {
-            for (int i = 0; i < dims.length; i++)
-                dimv[i] = dims[i];
-
-            this.data = null;
-            this.array = displacedTo;
-            this.displacement = displacement;
-            this.totalSize = computeTotalSize(dims);
-
-            return this;
-        } else {
-            ComplexArray_ByteBuffer a = new ComplexArray_ByteBuffer(dims, displacedTo, displacement);
-
-            return a;
-        }
-    }
+  }
 }
