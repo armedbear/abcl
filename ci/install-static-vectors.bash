156e3a8847b3f8e0cb7a8ca2d4e0ee633048b3e9
Mark <evenson.not.org@gmail.com>
2020-06-03 10:33:27 +0200

    ci: test STATIC-VECTORS
    Tests <https://github.com/armedbear/static-vectors/tree/abcl/easye-20200603a>.

---
+++ ci/install-static-vectors.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+dir="static-vectors"
+uri="https://github.com/armedbear/${dir}"
+root="${HOME}/quicklisp/local-projects"
+tag="abcl/easye-20200603a"
+
+mkdir -p ${root}
+pushd ${root}
+
+if [[ ! -d ${dir} ]]; then 
+    git clone ${uri} ${dir}
+fi
+
+pushd ${dir}
+if [[ -d .hg ]]; then
+    hg pull
+    hg update -r $tag
+    hg sum -v
+else
+    git pull
+    git checkout $tag
+    git show-ref
+    git rev-parse
+fi
+popd
+
+popd
