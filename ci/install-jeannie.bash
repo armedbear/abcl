6337542ec035cdf281d4676b37f887836b7dfe86
Mark <evenson.not.org@gmail.com>
2020-06-04 15:42:23 +0200

    ci: Use Jeannie to test <urn:abcl.org/**/*> namespace

---
+++ ci/install-jeannie.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+dir="jeannie"
+uri="https://github.com/easye/${dir}"
+root="${HOME}/quicklisp/local-projects"
+tag="master"
+
+mkdir -p ${root}
+pushd ${root}
+
+if [[ ! -d ${dir} ]]; then 
+    git clone ${uri} ${dir}
+fi
+
+pushd ${dir}
+if [[ -d .hg ]]; then
+    hg pull
+    hg update -r $tag
+    hg sum -v
+else
+    git pull
+    git checkout $tag
+    git show-ref
+    git rev-parse
+fi
+popd
+
+popd
