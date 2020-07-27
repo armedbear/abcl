d60eb3df8ad813de3d55623abd6512ca9691130e
Mark <evenson.not.org@gmail.com>
2020-07-27 15:42:19 +0200

    t: test basic sys:run-program
    <https://abcl.org/trac/ticket/470>

---
+++ t/sys-run-program.lisp
@@ -0,0 +1,15 @@
+(prove:plan 1)
+(prove:isnt
+ (handler-case
+     (let ((result 
+             (sys:run-program "true" nil)))
+       result)
+   (error (e)
+     (prog1
+         nil
+       (prove:diag (format nil "error: ~a" e)))))
+ nil
+ "Invoking sys:run-program")
+     
+(prove:finalize)
+
