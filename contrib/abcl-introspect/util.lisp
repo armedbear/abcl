866ca919814c041b25d85f3635cfdd6c5ed7903f
Mark <evenson.not.org@gmail.com>
2020-07-30 14:06:16 +0200

    abcl-introspect: add convenience method to read class files
    e.g.
    
        (abcl-introspect/jvm/tools/javap:disassemble-class-bytes
            (ext:read-class
                    "~//work/abcl/build/classes/org/armedbear/lisp/run_program_60.cls"))
    
    TODO:  shorten into generic method

--- contrib/abcl-introspect/util.lisp
+++ contrib/abcl-introspect/util.lisp
@@ -5,9 +5,24 @@
   "Write the Java byte[] array CLASS-BYTES to PATHNAME."
   (with-open-file (stream pathname
                           :direction :output
-                          :element-type '(signed-byte 8))
+                          :element-type '(unsigned-byte 8))
     (dotimes (i (java:jarray-length class-bytes))
       (write-byte (java:jarray-ref class-bytes i) stream))))
 
-(export '(write-class) :extensions)
+(defun read-class (pathname)
+  "Read the file at PATHNAME as a Java byte[] array"
+  (with-open-file (stream pathname
+                          :direction :input
+                          :element-type '(unsigned-byte 8))
+    (let* ((length
+             (file-length stream))
+           (array
+             (make-array length :element-type '(unsigned-byte 8))))
+      (read-sequence array stream :end length)
+      (java:jnew-array-from-array "byte" array))))
+  
+  
+(export '(write-class
+          read-class)
+        :extensions)
 
