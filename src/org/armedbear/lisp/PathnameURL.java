8c22aeefa23cefad538274ed0b4d3f4a3a007685
Mark <evenson.not.org@gmail.com>
2020-09-19 15:57:16 +0200

    Make Pathname.init() static method
    Running under Netbeans, but failing to enumerate jar pathnames in the
    Java tests.
    * * *
    Moved (most) Pathname.truename() invocations to using symbols
    
    Changed a lot of use of PROBE-FILE over TRUENAME.
    
    Running under Netbeans, but can't probe-file on jar locations:
    
      (probe-file "jar:file:/opt/local/share/java/abcl/abcl.jar!/org/armedbear/lisp/")
      (probe-file "jar:file:/opt/local/share/java/abcl/abcl.jar!/org/armedbear/lisp/boot.lisp")
    * * *
    INCOMPLETE Overhaul ZipCache to give linear access to PathnameURL keys
    
    TODO: comprehensive multi-level cache design that will plausibly work
    * * *
    Compiling!
    
    Loading from boot jar not working.
    
    Reading all the jar entries into the entry cache at once is not a good
    idea.  For abcl.jar this is ~10k entries which takes a noticable
    amount of time.
    
    And PathnameJar does not implement the right equals() contract, so
    looking up the entry in the cache doesn't work either.
    * * *
    Running from abcl.jar!
    
    Not loading in contribs…
    * * *
    Good progress: able to load from abcl.jar.
    
    Failing to bootstrap contrib due to the following errors.
    **** TODO Failing to distinguish that org/armedbear/lisp/java *doesn't* name a directory
    I thought ZIP files did not suffix directories with a slash?
    * * *
    DIRECTORY partly working for jar archives
    
    But
    
      (directory "jar:file:~/work/abcl/dist/abcl.jar!/*")
    
    returns all contents of the jar.
    * * *
    Use java.net.URI to encode namestrings

--- src/org/armedbear/lisp/PathnameURL.java
+++ src/org/armedbear/lisp/PathnameURL.java
@@ -1,5 +1,5 @@
 /* 
- * PathnameJar.java
+ * PathnameURL.java
  *
  * Copyright (C) 2020 @easye
  *
@@ -35,13 +35,17 @@ package org.armedbear.lisp;
 import static org.armedbear.lisp.Lisp.*;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URI;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.text.MessageFormat;
 
-public class PathnameURL extends Pathname {
+public class PathnameURL
+  extends Pathname
+{
   static final Symbol SCHEME = internKeyword("SCHEME");
   static final Symbol AUTHORITY = internKeyword("AUTHORITY");
   static final Symbol QUERY = internKeyword("QUERY");
@@ -146,9 +150,10 @@ public class PathnameURL extends Pathname {
     } 
 
     Pathname p = (Pathname)Pathname.create(path != null ? path : ""); 
-    result.setDirectory(p.getDirectory());
-    result.setName(p.getName());
-    result.setType(p.getType());
+    result
+      .setDirectory(p.getDirectory())
+      .setName(p.getName())
+      .setType(p.getType());
 
     return result;
   }
@@ -202,8 +207,48 @@ public class PathnameURL extends Pathname {
   }
 
   public String getNamestringAsURI() {
-    URL url = makeURL(this);
-    return url.toString();
+    LispObject schemeProperty = Symbol.GETF.execute(getHost(), SCHEME, NIL);
+    LispObject authorityProperty = Symbol.GETF.execute(getHost(), AUTHORITY, NIL);
+    LispObject queryProperty = Symbol.GETF.execute(getHost(), QUERY, NIL);
+    LispObject fragmentProperty = Symbol.GETF.execute(getHost(), FRAGMENT, NIL);
+
+    String scheme;
+    String authority;
+    if (!schemeProperty.equals(NIL)) {
+      scheme = schemeProperty.getStringValue();
+      authority =  authorityProperty.getStringValue();
+    } else {
+      scheme = "file";
+      authority = "";
+    }
+
+    String directory = getDirectoryNamestring();
+    String file = Symbol.FILE_NAMESTRING.execute(this).getStringValue();
+    String path = "";
+
+    if (!directory.equals("")) {
+      path = directory + file;
+    } else {
+      path = file;
+    }
+
+    String query = null;
+    if (!queryProperty.equals(NIL)) {
+      query = queryProperty.getStringValue();
+    }
+
+    String fragment = null;
+    if (!fragmentProperty.equals(NIL)) {
+      fragment = fragmentProperty.getStringValue();
+    }
+
+    try {
+      URI uri = new URI(scheme, authority, path, query, fragment);
+      return uri.toString();
+    } catch (URISyntaxException e) {
+      simple_error("Failed to construct a URI: ~a", this);
+      return (String)UNREACHED;
+    }
   }
 
   public LispObject typeOf() {
@@ -216,6 +261,11 @@ public class PathnameURL extends Pathname {
   }
 
   public static LispObject truename(PathnameURL p, boolean errorIfDoesNotExist) {
+    if (p.getHost().equals(NIL)
+        || Symbol.GETF.execute(p.getHost(), PathnameURL.SCHEME, NIL).equals("file")) {
+      return Pathname.truename((Pathname)p, errorIfDoesNotExist);
+    }
+        
     if (p.getInputStream() != null) {
       // If there is no type, query or fragment, we check to
       // see if there is URL available "underneath".
@@ -231,5 +281,84 @@ public class PathnameURL extends Pathname {
     }
     return Pathname.doTruenameExit(p, errorIfDoesNotExist);
   }
+
+  public InputStream getInputStream() {
+    InputStream result = null;
+
+    URL url = this.toURL();
+    try { 
+      result = url.openStream();
+    } catch (IOException e) {
+      Debug.warn("Failed to get InputStream from "
+                 + "'" + getNamestring() + "'"
+                 + ": " + e);
+    }
+    return result;
+  }
+
+  public long getLastModified() {
+    return getURLConnection().getLastModified();
+  }
 }
                                  
+
+
+// From Pathname.init()
+        // A URL 
+        // if (isValidURL(s)) {
+        //     URL url = null;
+        //     try {
+        //         url = new URL(s);
+        //     } catch (MalformedURLException e) {
+        //         Debug.assertTrue(false);
+        //     }
+        //     String scheme = url.getProtocol();
+        //     if (scheme.equals("file")) {
+        //         URI uri = null;
+        //         try {
+        //             uri = new URI(s);
+        //         } catch (URISyntaxException ex) {
+        //             error(new SimpleError("Improper URI syntax for "
+        //                             + "'" + url.toString() + "'"
+        //                             + ": " + ex.toString()));
+        //         }
+            
+        //         String uriPath = uri.getPath();
+        //         if (null == uriPath) {
+        //                           // Under Windows, deal with pathnames containing
+        //                           // devices expressed as "file:z:/foo/path"
+        //                           uriPath = uri.getSchemeSpecificPart();
+        //                           if (uriPath == null || uriPath.equals("")) {
+        //             error(new LispError("The URI has no path: " + uri));
+        //           }
+        //         }
+        //         final File file = new File(uriPath);
+        //         String path = file.getPath();
+        //         if (uri.toString().endsWith("/") && !path.endsWith("/")) {
+        //           path += "/";
+        //         }
+        //         final Pathname p = (Pathname)Pathname.create(path);
+        //         this.setHost(p.getHost());
+        //         this.setDevice(p.getDevice());
+        //         this.setDirectory(p.getDirectory());
+        //         this.setName(p.getName());
+        //         this.setType(p.getType());
+        //         this.setVersion(p.getVersion());
+        //         return;
+        //     }
+        //     Debug.assertTrue(scheme != null);
+        //     URI uri = null;
+        //     try { 
+        //         uri = url.toURI().normalize();
+        //     } catch (URISyntaxException e) {
+        //         error(new LispError("Couldn't form URI from "
+        //                             + "'" + url + "'"
+        //                             + " because: " + e));
+        //     }
+        //     String authority = uri.getAuthority();
+        // if (authority == null) {
+        //   authority = url.getAuthority();
+        //   if (authority == null) {
+        //     Debug.trace(MessageFormat.format("{0} has a null authority.", url));
+        //   }
+        // }
