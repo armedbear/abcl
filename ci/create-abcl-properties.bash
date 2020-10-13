ed843b7949b9c6232032b949f610a257a29d4da5
Mark <evenson.not.org@gmail.com>
2020-10-13 09:48:44 +0200

    build: make building and running a local ABCL easier
    (From a suggestion by Slyrus)
    
    Replace use of abcl.javac.{source,target} with the "magic"
    ant.build.javac,{source,target} properties.
    
    Explicitly configure 'abcl.release' target to use openjdk8 compilation.
    
    The Java compilation options are perhaps underspecified in terms of
    the target JVM in order to support a developer who simply wants to
    compile and run locally with least hassles.
    
    When preparing ABCL binaries for wider distribution, the values of the
    abcl.build.target.javac and abcl.build.source.java become more
    important.
    
    The 'abcl.properties.autoconfigure.*' targets use the
    ci/create-build-properties.bash script to set these options for
    various openjdk platforms.
    * * *
    build: build the wrapper with the release
    
    N.b. the wrapper is a local artifact, not supposed to be shipped with
    the release.

--- ci/create-abcl-properties.bash
+++ ci/create-abcl-properties.bash
@@ -18,52 +18,52 @@ abcl_javac_source=1.8
 case $jdk in
     6|openjdk6)
         options="-d64 -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=1g -XX:+UseConcMarkSweepGC"
-        abcl_javac_target=1.6
-	abcl_javac_source=1.6
+        ant_build_javac_target=1.6
+	ant_build_javac_source=1.6
         ;;
     7|openjdk7)
 	options="-d64 -XX:+UseG1GC"
-        abcl_javac_target=1.7
-	abcl_javac_source=1.7
+        ant_build_javac_target=1.7
+	ant_build_javac_source=1.7
 	;;
     8|openjdk8)
         options="-XX:+UseG1GC -XX:+AggressiveOpts -XX:CompileThreshold=10"
-	abcl_javac_target=1.8
-	abcl_javac_source=1.8
+	ant_build_javac_target=1.8
+	ant_build_javac_source=1.8
         ;;
     11|openjdk11)
         options="-XX:CompileThreshold=10"
-	abcl_javac_target=11
-	abcl_javac_source=1.8
+	ant_build_javac_target=11
+	ant_build_javac_source=1.8
         ;;
     # untested: weakly unsupported 
     12|openjdk12)
         options="-XX:CompileThreshold=10"
-	abcl_javac_target=12
-	abcl_javac_source=1.8
+	ant_build_javac_target=12
+	ant_build_javac_source=1.8
         ;;
     13|openjdk13)
         options="-XX:CompileThreshold=10"
-	abcl_javac_target=13
-	abcl_javac_source=1.8
+	ant_build_javac_target=13
+	ant_build_javac_source=1.8
         ;;
     14|openjdk14)
         options="-XX:CompileThreshold=10 ${zgc}"
-	abcl_javac_target=14
-	abcl_javac_source=1.8
+	ant_build_javac_target=14
+	ant_build_javac_source=1.8
         ;;
     15|openjdk15)
         options="-XX:CompileThreshold=10 ${zgc}"
-	abcl_javac_target=15
-	abcl_javac_source=1.8
+	ant_build_javac_target=15
+	ant_build_javac_source=1.8
         ;;
 esac
 
 cat ${root}/abcl.properties.in \
     | awk -F = \
 	  -v options="$options" \
-	  -v target="$abcl_javac_target" \
-	  -v source="$abcl_javac_source" \
+	  -v target="$ant_build_javac_target" \
+	  -v source="$ant_build_javac_source" \
        -f ${DIR}/create-abcl-properties.awk \
   > ${root}/abcl.properties
 
