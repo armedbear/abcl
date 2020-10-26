e70866f6235a907ea09240d611b428c0c476da5e
Mark <evenson.not.org@gmail.com>
2020-10-26 21:36:08 +0100

    abcl 1.8.0 metadata
    rc5
    
    TODO around pathname, manual needs further

--- src/org/armedbear/lisp/Version.java
+++ src/org/armedbear/lisp/Version.java
@@ -41,7 +41,7 @@ public final class Version
 {
   private Version() {}
   
-  static final String baseVersion = "1.7.2-dev";
+  static final String baseVersion = "1.8.0";
   
   static void init() {
     try {
