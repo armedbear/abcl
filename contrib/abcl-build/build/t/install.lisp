a4dcd3251d95fcc856ec28fa6e119c59353d1e93
Mark <evenson.not.org@gmail.com>
2020-11-03 08:11:49 +0100

    t/abcl-build: update ant test download uri

--- contrib/abcl-build/build/t/install.lisp
+++ contrib/abcl-build/build/t/install.lisp
@@ -1,20 +1,21 @@
 (in-package :cl-user)
 
-(prove:plan 1)
-(prove:ok 
- (abcl/build:xdg/abcl-install-root #P"http://archive.apache.org/dist/ant/binaries/apache-ant-1.9.4-bin.zip"))
+(let ((uri #p"https://downloads.apache.org/ant/binaries/apache-ant-1.10.9-bin.zip"))
+  (prove:plan 1)
+  (prove:ok 
+   (abcl/build:xdg/abcl-install-root uri)
+   (format nil "Suitable install root for <~a>" uri))
 
-(prove:plan 2)
-(let ((path (ext:make-temp-directory))
-      (uri #p"http://www-eu.apache.org/dist/ant/binaries/apache-ant-1.10.7-bin.zip"))
-  (prove:diag
-   (format nil "Testing binary unzip installation of~%~,2t<~a>~%to~%~,2t '~a'." uri path))
-  (multiple-value-bind (root contents)
-      (abcl/build:xdg/install uri)
-    (prove:ok (and root
-                   (probe-file root)))
-    (prove:ok (and
-               (consp contents)
-               (> (length contents) 0)))))
+   (prove:plan 2)
+   (let ((path (ext:make-temp-directory)))
+         (prove:diag
+           (format nil "Testing binary unzip installation of~%~,2t<~a>~%to~%~,2t '~a'." uri path))
+         (multiple-value-bind (root contents)
+             (abcl/build:xdg/install uri)
+           (prove:ok (and root
+                          (probe-file root)))
+           (prove:ok (and
+                      (consp contents)
+                      (> (length contents) 0))))))
 (prove:finalize)
 
