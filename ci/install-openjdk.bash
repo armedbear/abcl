62cd0d8f04dff98ba5b93ca836422af29fa5f216
Mark Evenson <evenson.not.org@gmail.com>
2022-11-28 10:13:35 +0100

    ci: add openjdk19 installation
    TODO: re-factor the rest of the downloads, assuming that Eclipse has
    their stuff together over openjdk package naming (finally?!)

--- ci/install-openjdk.bash
+++ ci/install-openjdk.bash
@@ -8,7 +8,7 @@ if [[ -z $jdk ]]; then
     jdk=openjdk8
 fi
 
-n# empty variables are not necessary, but a hint that these are not
+# empty variables are not necessary, but a hint that these are not
 # lexically scoped in their modification.
 topdir=
 dist=
@@ -44,6 +44,15 @@ function determine_openjdk() {
                     topdir="jdk-17.0.5+8"
                     dist="https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.5%2B8/OpenJDK17U-jdk_x64_mac_hotspot_17.0.5_8.tar.gz"
                     ;;
+                openjdk19)
+                    v="19"
+                    id="${v}.0.1"
+                    rev="10"
+                    arch="jdk_x64_mac_hotspot"
+                    topdir="jdk${id}+${rev}"
+                    dist="https://github.com/adoptium/temurin${v}-binaries/releases/download/jdk-${id}%2B${rev}/OpenJDK${v}U-${arch}_${id}_${rev}.tar.gz"
+                    ;;
+
             esac
             ;;
         Linux)
@@ -74,6 +83,15 @@ function determine_openjdk() {
                     topdir="jdk-17.0.5+8"
                     dist="https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.5%2B8/OpenJDK17U-jdk_x64_linux_hotspot_17.0.5_8.tar.gz"
                     ;;
+                openjdk19)
+                    v="19"
+                    id="${v}.0.1"
+                    rev="10"
+                    arch="jdk_x64_linux_hotspot"
+                    topdir="jdk${id}+${rev}"
+                    dist="https://github.com/adoptium/temurin${v}-binaries/releases/download/jdk-${id}%2B${rev}/OpenJDK${v}U-${arch}_${id}_${rev}.tar.gz"
+                    ;;
+
 esac
             ;;
         *)
