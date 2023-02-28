1caa68a792b26de1f7c5d841091b924cde64c610
Mark Evenson <evenson.not.org@gmail.com>
2023-02-28 14:15:32 +0100

    ci: tighten Jeannie test setup in stand-alone usage

--- ci/test-jeannie.lisp
+++ ci/test-jeannie.lisp
@@ -1,6 +1,7 @@
 ;;; Jeannie is not in Quicklisp: see <file:install-jeannie.bash>
 ;;; But we possibly need to install dependencies from Quicklisp
-(ql:quickload :jeannie)
+(unless (ignore-errors (asdf:make :jeannie/test))
+  (ql:quickload :jeannie/test))
 
 (time 
  (asdf:test-system :jeannie))
