0fee9ea3c9506b7ed95f40447070c628fe2a0d2f
Mark Evenson <evenson.not.org@gmail.com>
2023-03-21 09:04:42 -0400

    ci: improve success chances for ABCL-INTROSPECT test

--- ci/test-abcl-introspect.lisp
+++ ci/test-abcl-introspect.lisp
@@ -1,4 +1,9 @@
-(ql:quickload :abcl-introspect-test)
+(require :abcl-contrib)
+(asdf:make :quicklisp-abcl)
+
+(unless (ignore-errors
+         (asdf:make :abcl-introspect-test))
+  (ql:quickload :abcl-introspect-test))
 
 (time 
  (asdf:test-system :abcl-introspect))
