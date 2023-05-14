256c090954aaeabdda9ed42507b1166250448b1e
Mark Evenson <mevenson@retch.slack.net>
2023-05-14 12:53:07 +0000

    ci: fix initialization of abcl-contrib test

--- ci/test-abcl-contrib.lisp
+++ ci/test-abcl-contrib.lisp
@@ -1,9 +1,13 @@
 (require :asdf)
 (require :abcl-contrib)
+(asdf:make :quicklisp-abcl)
 
-(dolist (system '(:jss :asdf-jar))
-  (asdf:make system)
+(dolist (system '(:jss-tests :asdf-jar-test))
+  (unless
+      (ignore-errors (asdf:make system))
+    (ql:quickload system))
   (time 
    (asdf:test-system system)))
+
 
 
