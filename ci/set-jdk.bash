a9d66c958d6fd90ea2d051dede32f8df6256ad79
Mark Evenson <evenson.not.org@gmail.com>
2023-05-31 10:48:27 +0200

    ci: update openjdk versions
    Deprecate openjdk19 in favor of openjdk20 as it is the current
    development release.
    
    Have <file:ci/set-jdk.bash> only use its arguments to set jdk
    version, while attempting to be more verbose and guard against failure
    to interpolate these values as we go.
    
    Usage:
    
        set-jdk OPENJDK_VERSION ABCL_ROOT
    
    where
    
    OPENJDK_VERSION is of the form 'openjdk8', 'openjdk19', etc.
    
    ABCL_ROOT is the root of the ABCL source tree

--- ci/set-jdk.bash
+++ ci/set-jdk.bash
@@ -4,39 +4,61 @@ function set_jdk() {
         abcl_jdk=openjdk8
     fi
 
+    echo Configuring JDK from ${abcl_jdk}
+
     dir=$2
     if [[ -z ${dir} ]]; then
         dir=${ABCL_ROOT}
     fi
 
+    if [[ -z ${ABCL_ROOT} ]]; then
+        dir=/var/tmp/set-jdk.$$/
+    fi
+
     . ${DIR}/ensure-jenv-is-present.bash
 
-    jenv versions
+    echo Available jenv versions $(jenv versions)
 
     case ${abcl_jdk} in
         openjdk8)
-            version=$(jenv versions | grep openjdk | grep 1.8 | tail -1 | sed s/*//)
+            version=$(jenv versions | grep ^\ *1\.8\.[0-9] | tail -1 | sed s/*//)
             ;;
         openjdk11)
-            version=$(jenv versions | grep openjdk | grep 11.0 | tail -1 | sed s/*//)
+            version=$(jenv versions | grep ^\ *11\.[0-9] | tail -1 | sed s/*//)
             ;;
+        openjdk17)
+            version=$(jenv versions | grep ^\ *17\.[0-9] | tail -1 | sed s/*//)
+            ;;
+        openjdk17)
+            version=$(jenv versions | grep ^\ *19\.[0-9] | tail -1 | sed s/*//)
+            ;;
+        openjdk20)
+            version=$(jenv versions | grep ^\ *20\.[0-9] | tail -1 | sed s/*//)
+            ;;
+        *)
+            echo Failed to find an available JDK matching ${abcl_jdk}
+            echo   in $(jenv versions)
+            echo
+            echo Falling back to grabbing the last one listed
+            version=$(jenv versions | tail -1 | sed s/*//)
     esac
 
-    if [[ -z ${version} ]]; then
-        version=$(jenv versions | tail -1 | sed s/*//)
+    if [[ ! -d ${dir} ]]; then
+        echo Creating directory ${dir}
+        mkdir ${dir}
     fi
 
-    if [[ -z ${version} ]]; then
-        version=1.8
-    fi
-    
     pushd ${dir}
 
-    jenv local ${version}
+    if [[ 0 -ne $(jenv local ${version}) ]]; then 
+       echo Failed to set local JDK to ${version}
+    fi 
     # but practically we guard every invocation of jenv this way
-    jenv global ${version}
+    if [[ 0 -ne $(jenv global ${version}) ]]; then 
+       echo Failed to set global JDK to ${version}
+    fi 
 
-    jenv version
+    echo Local version to set $(jenv version)
 
     popd
 }
