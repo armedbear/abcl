b2d310afa5d6199d48d59ab2910fd66bfd55b29b
Alejandro Zamora <ale2014.zamora@gmail.com>
2023-07-10 11:29:25 -0300

    Fix for procyon decompiler (for cl:disassemble)
    - This makes procyon disassembler work in ABCL, internally is using a
    temp file for the byte array received, then the tempfile is passed to
    the decompiler.
    - Updated decompiler status info

--- contrib/abcl-introspect/procyon.lisp
+++ contrib/abcl-introspect/procyon.lisp
@@ -4,9 +4,22 @@
      #:disassemble-class-bytes))
 (in-package :abcl-introspect/jvm/tools/procyon)
 
+
+(defun bytes->temp-file (bytes)
+  (let* ((create-temp-file-method
+           (java:jmethod "java.io.File" "createTempFile"
+                         "java.lang.String" "java.lang.String" "java.io.File"))
+         (write-method (java:jmethod "java.io.FileOutputStream" "write" "byte[]"))
+         (temp-file (java:jstatic create-temp-file-method "java.io.File"
+                                  (symbol-name (gensym "classfile")) ".class" java:+null+))
+         (temp-file-output-stream (java:jnew "java.io.FileOutputStream" temp-file))
+         (to-string-method (java:jmethod "java.lang.Object" "toString")))
+    (java:jcall write-method temp-file-output-stream bytes)
+    (java:jcall to-string-method temp-file)))
+
 (defun disassemble-class-bytes (object)
   #|
-  <https://bitbucket.org/mstrobel/procyon/wiki/Decompiler%20API> 
+  <https://bitbucket.org/mstrobel/procyon/wiki/Decompiler%20API>
 
     final DecompilerSettings settings = DecompilerSettings.javaDefaults();
 
@@ -23,27 +36,19 @@
         // handle error
     }
   |#
-  (let* ((settings
-           (#"javaDefaults" 'DecompilerSettings))
-         (writer
-           (jss:new 'java.io.StringWriter)))
-    (#"decompile" 'Decompiler
-                  ;;; !!! need to reference as a type in the current VM
-                  ;;; c.f.<https://github.com/Konloch/bytecode-viewer/blob/master/src/the/bytecode/club/bytecodeviewer/decompilers/ProcyonDecompiler.java>
-                  object
-                  (jss:new 'PlainTextOutput writer)
-                  settings)
-    (write (#"toString writer"))))
+  (let ((decompile-method (java:jmethod "com.strobel.decompiler.Decompiler"
+                                        "decompile" "java.lang.String"
+                                        "com.strobel.decompiler.ITextOutput"))
+        (string-writer (java:jnew "java.io.StringWriter")))
+    (java:jstatic decompile-method
+                  "com.strobel.decompiler.Decompiler"
+                  (bytes->temp-file object)
+                  (java:jnew "com.strobel.decompiler.PlainTextOutput" string-writer))
+    (java:jcall (java:jmethod "java.io.StringWriter" "toString") string-writer)))
+
+
 
 (eval-when (:load-toplevel :execute)
   (pushnew `(:procyon . abcl-introspect/jvm/tools/procyon::disassemble-class-bytes)
            sys::*disassemblers*)
   (format cl:*load-verbose* "~&; ~a: Successfully added procyon disassembler.~%" *package*))
-
-
-
-
-
-
-  
-  
