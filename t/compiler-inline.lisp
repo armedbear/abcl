8df47c02ee3ad808ccd241e936c679e431045ebc
Mark Evenson <evenson.not.org@gmail.com>
2023-07-15 08:50:23 +0200

    Improve test verifying it succeeds in SBCL and ECL
    Be more careful about packages

--- t/compiler-inline.lisp
+++ t/compiler-inline.lisp
@@ -2,20 +2,13 @@
 (unless (ignore-errors (asdf:load-system :fset))
   (ql:quickload :fset))
 
-(in-package :fset-user)
-(defun foo (x)
-  (labels ((bar (x)
-             (cond ((null x) nil)
-                   ((eq (car x) '&rest) (tail (cadr x)))
-                   (t (cons (car x) (bar (cdr x))))))
-           (tail (x)
-             (list 'local-tail x)))
-    (bar x)))
-(compile 'foo)
+(load 
+ (compile-file
+  (asdf:system-relative-pathname :abcl "t/eg/inline-labels.lisp")))
 
 (prove:plan 1)
 (prove:is
-    (foo '(a &rest b))
-    '(a local-tail b))
+    (fset-user::foo '(a &rest b))
+    '(a fset-user::local-tail b))
 (prove:finalize)
 
