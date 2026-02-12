5f677c6babb2f58e05cb556e2ec04eb51adca1da
Mark Evenson <evenson.not.org@gmail.com>
2026-02-12 13:22:24 +0100

    ci: fix ansi-test install location for github.com runner
    Work around github.com CI funkiness.
    
    ABCL_ROOT should be set to the location of the abcl source, and the
    runners should be able to install ansi-test in a sibling directory.

--- ci/install-ansi-test.bash
+++ ci/install-ansi-test.bash
@@ -3,14 +3,21 @@
 uri="https://gitlab.common-lisp.net/ansi-test/ansi-test"
 commitish="master"
 
-topdir="$(dirname ${ABCL_ROOT:-/tmp}/..)"
+# work around (as yet) unresolved GITHUB_WORKSPACE wonkiness
+# <https://github.com/actions/runner/issues/2058>
+if [[ -z "${ABCL_ROOT}" ]]; then
+    topdir=$(realpath /tmp)
+else     
+   topdir="$(realpath "${ABCL_ROOT}"/..)"
+fi
+   
 if [ ! -d "${topdir}" ]; then
   mkdir -p "${topdir}"
 fi 
 
 pushd "${topdir}" && echo "Cloning <${uri}> under <file://${topdir}>."
 if [[ ! -r ansi-test ]]; then
-    git clone "${uri}"
+    git clone "${uri}" ansi-test
 else
     pushd ansi-test
     if [[ -r .hg ]]; then
@@ -25,7 +32,7 @@ pushd ansi-test
 # DEPRECATED pin known working version
 # <https://gitlab.common-lisp.net/ansi-test/ansi-test/-/issues/31>
 # git checkout 1c832cf
-echo "Explicitly updating <file://${topdir}> worktree to <commit:${commitish}>." \
+echo "Explicitly updating <file://${PWD}> worktree to <commit:${commitish}>." \
     && git checkout  "$commitish"
 echo "References to <commit:${commitish}>: " && git show-ref "$commitish"
 popd 
